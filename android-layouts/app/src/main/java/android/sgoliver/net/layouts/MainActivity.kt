8c484ad38cd68b2054f2025d8c72ee21c4e0879c
Salvador Gomez Oliver <sgoliver.net@gmail.com>
2020-04-26 20:47:59 +0200

    Nuevo proyecto: Layouts

---
+++ android-layouts/app/src/main/java/android/sgoliver/net/layouts/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.sgoliver.net.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_linearlayout)
+    }
+}
