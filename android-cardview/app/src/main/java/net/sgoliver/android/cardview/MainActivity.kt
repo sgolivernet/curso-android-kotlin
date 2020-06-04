f7e95dd4f6c1b1a5b6f7661396539cd653b6a3c5
Salvador Gomez Oliver <sgoliver.net@gmail.com>
2020-06-04 20:03:07 +0200

    Nuevo proyecto: cardview

---
+++ android-cardview/app/src/main/java/net/sgoliver/android/cardview/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.sgoliver.android.cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
